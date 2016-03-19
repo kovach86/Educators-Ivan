3743fc629a27ab3c1d070bd83498a19bc89152ec
kovach86 <ikovacevic86@gmail.com>
2016-03-19 13:54:29 +0100

    Added files via upload

---
+++ ErrorLogger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextHandlers
+{
+    public static class ErrorLogger
+    {
+        public static void WriteError(Exception ex)
+        {
+            string path = @"C:\Users\Kovach\Documents\Visual Studio 2015\Projects\Educators\TextHandlers\Text_Files\ErrorLogger.txt";
+            TextWriter writer = new StreamWriter(path, true);
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("------------------*----------------------");
+            sb.AppendLine(DateTime.Now + " Exception: ");
+            sb.AppendLine(ex.Message);
+            if (ex.InnerException != null)
+            {
+                sb.AppendLine("- - - - - - - ");
+                sb.AppendLine("Details:");
+                sb.AppendLine(ex.InnerException.Message);
+                sb.AppendLine(ex.InnerException.StackTrace);
+            }
+
+            writer.Write(sb.ToString());
+
+            writer.Flush();
+            writer.Close();
+        }
+    }
+}
---
+++ PhraseTranslator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TextHandlers
+{
+        public class PhraseTranslator
+        {
+            public string GetTranslation(string phrase_id, string language, string for_phrase)
+            {
+                XElement root = XElement.Load(@"C:\Users\Kovach\Desktop\test.xml");
+                return root.Elements(phrase_id).First()
+                    .Nodes().Cast<XElement>()
+                    .First(x => x.FirstAttribute.Value == language).Value;
+            }
+        }
+    }
+ 
