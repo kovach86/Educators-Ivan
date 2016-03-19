5bd6a3ce7bb0dbdcc08c707128e3d7eb621030e3
kovach86 <ikovacevic86@gmail.com>
2016-03-19 13:38:07 +0100

    Added files via upload

---
+++ BaseRepository.cs
@@ -0,0 +1,91 @@
+﻿using Domains;
+using TextHandlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate;
+using System.IO;
+
+namespace Repositories
+{
+    public class BaseRepository<T>:NHibernateBase,IBaseRepository<T> where T :class
+    {
+        public IList<T> GetAll()
+        {
+            using (_session = sessionFactory.OpenSession())
+            {
+                return _session.CreateCriteria(typeof(T)).List<T>();
+            }
+        }
+
+        public virtual string Add(T entity)
+        {
+            using (_session=sessionFactory.OpenSession())
+            {
+                try
+                {
+                    _session.Save(entity);
+                    return "ok";
+                }
+                catch (Exception ex)
+                {
+                    ErrorLogger.WriteError(ex);
+                    return "error";
+                }
+            }
+        }
+
+        public string Update(T entity)
+        {
+            using (_session = sessionFactory.OpenSession())
+            {
+                try
+                {
+                    _session.SaveOrUpdate(entity);
+                    return "ok";
+                }
+                catch (Exception ex)
+                {
+                    ErrorLogger.WriteError(ex);
+                    return "error";
+                }
+            }
+        }
+
+        public string Delete(T entity)
+        {
+            using (_session = sessionFactory.OpenSession())
+            {
+                try
+                {
+                    _session.Delete(entity);
+                    return "ok";
+                }
+                catch (Exception ex)
+                {
+                    ErrorLogger.WriteError(ex);
+                    return "error";
+                }
+            }
+        }
+
+        public T FindById(int id)
+        {
+            using (_session = sessionFactory.OpenSession())
+            {
+                try
+                {
+                    return _session.Get<T>(id);
+                }
+                catch (Exception ex)
+                {
+                    ErrorLogger.WriteError(ex);
+                    return null;
+                }
+            }
+        }
+        
+    }
+}
---
+++ IBaseRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public interface IBaseRepository<TEntity> where TEntity:class
+    {
+        #region CRUD and oher basic operations
+        IList<TEntity> GetAll();
+        string Add(TEntity entity);
+        string Update(TEntity entity);
+        string Delete(TEntity entity);
+        TEntity FindById(int id);
+        #endregion
+       
+    }
+}
