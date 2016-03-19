12c5b670cb49460f842b3e740a424a08c4fa2667
kovach86 <ikovacevic86@gmail.com>
2016-03-19 14:02:34 +0100

    Added files via upload

---
+++ MailSender.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextHandlers
+{
+    public class MailSender
+    {
+        public string UserName { get; set; }
+
+        public string SendEmail()
+        {
+            using (MailMessage mm = new MailMessage("ikovacevic86@gmail.com", "bojana.stojiljkovic@gmail.com"))
+            {
+                {
+                    mm.IsBodyHtml = true;
+                    mm.Body = "<html> <head></head> <body> <b>Evo malo texta </body> </b> ";
+                    mm.Subject = "Eiiiij";
+
+                    SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+                    smtp.EnableSsl = true;
+
+                    NetworkCredential nwc = new NetworkCredential(" ", " ");
+                    smtp.UseDefaultCredentials = true;
+                    smtp.Credentials = nwc;
+                    smtp.Port = 587;
+                    try
+                    {
+                        smtp.Send(mm);
+                        return "Send";
+                    }
+                    catch (Exception ex)
+                    {
+                        ErrorLogger.WriteError(ex);
+                        return "";
+                    }
+
+                }
+            }
+        }
+    }
+}
